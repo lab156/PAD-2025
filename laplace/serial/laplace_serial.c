5d2f6e46be153f04cf13fcae876ba0a9f40e17f8
Luis <lab156@case.edu>
2025-06-03 19:56:59 -0600

    Tue Jun  3 07:56:59 PM CST 2025

--- laplace/serial/laplace_serial.c
+++ laplace/serial/laplace_serial.c
@@ -61,8 +61,10 @@ int main(int argc, char *argv[]) {
         // main calculation: average my four neighbors
         for(i = 1; i <= ROWS; i++) {
             for(j = 1; j <= COLUMNS; j++) {
-                Temperature[i][j] = 0.25 * (Temperature_last[i+1][j] + Temperature_last[i-1][j] +
-                                            Temperature_last[i][j+1] + Temperature_last[i][j-1]);
+                Temperature[i][j] = 0.25 * (Temperature_last[i+1][j] 
+                        + Temperature_last[i-1][j]
+                        + Temperature_last[i][j+1] 
+                        + Temperature_last[i][j-1]);
             }
         }
         
