947d32696fda082068284addef0367a9b4e8a977
Luis <lab156@case.edu>
2025-06-18 21:01:17 -0600

    Wed Jun 18 09:01:17 PM CST 2025

---
+++ gradiente/regresion/generate_write.c
@@ -0,0 +1,43 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <stdlib.h>
+#include <string.h>
+#include "reg_utils.h"
+
+int main(int argc, char *argv[]) {
+
+    DataPoint *data_array;
+    int num_data = 1000000;
+
+    data_array = (DataPoint *)malloc(num_data * sizeof(DataPoint));
+
+    initialize_data(num_data, data_array, 1.63, -1.3);
+
+    // Open file for writing
+    // los modos de un archivo son 'r' read y 'w' write
+    FILE *file = fopen(argv[1], "w");
+    // Check for errors
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // Write the contents of data_array
+    for (int i=0; i<num_data; i++) {
+        fprintf(file, "%f, %f \n", data_array[i].x, data_array[i].y);
+        
+    }
+
+
+    /*
+    char line[1024];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+    */
+
+    // ALWAYS CLOSE THE FILE
+    fclose(file);
+    return 0;
+}
