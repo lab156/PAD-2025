58847b56a2560802b817a9e66e0435c73d58d4e7
Luis <lab156@case.edu>
2025-07-30 18:47:32 -0600

    Wed Jul 30 06:47:32 PM CST 2025

---
+++ lapack/solve.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <lapacke.h>
+
+// compile with 
+// gcc solve.c -o solve -L/path/to/lapack/libs -llapacke -llapack -lblas
+
+int main() {
+    int n = 3; // Matrix dimension
+    int nrhs = 1; // Number of right-hand sides
+    double A[] = {1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 9.0}; // Matrix A (column-major)
+    double B[] = {1.0, 2.0, 3.0}; // Right-hand side vector B
+    lapack_int ipiv[n]; // Pivot indices
+    lapack_int info;
+
+    // Solve the linear system
+    info = LAPACKE_dgesv(LAPACK_COL_MAJOR, n, nrhs, A, n, ipiv, B, n);
+
+    if (info == 0) {
+        printf("Solution x:\n");
+        for (int i = 0; i < n; i++) {
+            printf("%f\n", B[i]);
+        }
+    } else {
+        printf("LAPACKE_dgesv failed with info = %d\n", info);
+    }
+
+    return 0;
+}
