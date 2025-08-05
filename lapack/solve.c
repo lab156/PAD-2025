1e1b66f510a4b95e2b133a79bed27c5e571bd433
Luis <lab156@case.edu>
2025-08-04 20:37:29 -0600

    Mon Aug  4 08:37:29 PM CST 2025

--- lapack/solve.c
+++ lapack/solve.c
@@ -5,12 +5,13 @@
 int main() {
     int n = 3; // Matrix dimension
     int nrhs = 1; // Number of right-hand sides
-    double A[] = {1.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 9.0}; // Matrix A (column-major)
-    double B[] = {1.0, 2.0, 3.0}; // Right-hand side vector B
+    double A[] = {2.0, 4.0, 7.0, 2.0, 5.0, 8.0, 3.0, 6.0, 9.0}; // Matrix A (column-major)
+    double B[] = {6.0, 15.0, 24.0}; // Right-hand side vector B
     lapack_int ipiv[n]; // Pivot indices
     lapack_int info;
 
     // Solve the linear system
+    // LAPACK_COMPLEX_CPP significa que lea columna por columna
     info = LAPACKE_dgesv(LAPACK_COL_MAJOR, n, nrhs, A, n, ipiv, B, n);
 
     if (info == 0) {
