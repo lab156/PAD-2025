09f7fb14b255a119237ea7caae8728c3dd63745d
Luis <lab156@case.edu>
2025-08-06 17:23:48 -0600

    Wed Aug  6 05:23:48 PM CST 2025

---
+++ omplib/dynlib.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+void hello() {
+    printf("hola macizo\n");
+}
+
+void hi_omp() {
+    int private_id, shared_num = 6;
+# pragma omp parallel private(private_id) num_threads(shared_num)
+{
+private_id = omp_get_thread_num();
+printf(" shared_num = %d , private_id = %d  \n",
+ shared_num, private_id);
+#pragma omp master 
+printf(" Esta linea solo la ejecuta Master, %d \n", omp_get_thread_num());
+}
+}
---
+++ omplib/test.c
@@ -0,0 +1,8 @@
+#include"dynlib.h"
+
+int main() {
+
+    hello();
+    hi_omp();
+    return 0;
+}
