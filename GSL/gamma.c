08c79b7e19343ffe537c0de0fe2c949b43fe463c
Luis <lab156@case.edu>
2025-08-05 19:21:02 -0600

    Tue Aug  5 07:21:02 PM CST 2025

---
+++ GSL/gamma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <gsl/gsl_sf_gamma.h>
+
+int main() {
+    double x = 5.0;
+    double gamma_val = gsl_sf_gamma(x);
+    printf("Gamma(%f) = %f\n", x, gamma_val);
+
+    double lngamma_val = gsl_sf_lngamma(x);
+    printf("log(Gamma(%f)) = %f\n", x, lngamma_val);
+
+    return 0;
+}
